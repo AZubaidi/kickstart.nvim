9e42ca9c63f200dffe6efe6fa370dcf25f147de9
AZubaidi <abdulrahman.zubaidi@gmail.com>
2024-08-14 23:21:26 -0400

    emmet-ls files on php

--- init.lua
+++ init.lua
@@ -587,6 +587,7 @@ local servers = {
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  emmet_language_server = { filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" , 'php', 'blade.php' } },
 
   lua_ls = {
     Lua = {
